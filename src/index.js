5ea6882e72cb311e1eef35f587e3e262bcf531ae
Test User <test@test.com>
2026-03-31 20:45:43 -0400

    Mejorar headers de Wisp para evitar bloqueos de YouTube

--- src/index.js
+++ src/index.js
@@ -23,7 +23,16 @@ Object.assign(wisp.options, {
 	hostname_blacklist: [/example\.com/],
 	dns_servers: ["1.1.1.3", "1.0.0.3"],
 	headers: {
-		"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+		"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+	"accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+	"accept-language": "en-US,en;q=0.9",
+	"accept-encoding": "gzip, deflate, br",
+	"sec-fetch-dest": "document",
+	"sec-fetch-mode": "navigate",
+	"sec-fetch-site": "none",
+	"sec-fetch-user": "?1",
+	"upgrade-insecure-requests": "1",
+	"dnt": "1"
 	},
 });
 
